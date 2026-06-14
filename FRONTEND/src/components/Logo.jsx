c1434e0d583448a0dbf6f18392d17ef8db679bb4
PlayArenaTCC <adrianvinitccsixseven@gmail.com>
2026-06-14 18:03:19 -0300

    Delete FRONTEND prara restore

--- FRONTEND/src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import playarenaLogoUrl from '../../IMG/d2fd47a50_image-removebg-preview.png'
-
-export function Logo({ compact = false }) {
-  return (
-    <div className={compact ? 'logo logo-compact' : 'logo'}>
-      <img src={playarenaLogoUrl} alt="PlayArena" />
-    </div>
-  )
-}
